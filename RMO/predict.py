c2642813eeddacc74959d6b228d7146e87dfe9d7
OlehNovitskyi <oleh.novitskyi2022@gmail.com>
2026-01-27 14:26:05 +0100

    Added data + corrected summurizing func

--- RMO/predict.py
+++ RMO/predict.py
@@ -1,8 +1,8 @@
 from ultralytics import YOLO
 import tkinter as tk
 from tkinter import filedialog, messagebox
+from collections import Counter
 
-# ---- Callbacks ----
 model = YOLO("yolov11_customdata_trained.pt")
 monety_voc = {'1zl': 1, '2zl': 2, '5zl': 5}
 
@@ -34,13 +34,25 @@ def analyze_webcam():
 def run_model(mode, source):
     print(f"YOLO | mode={mode} | source={source}")
     if mode == "image":
-        results = model.predict(source=source, show=True, save=False)
+        results = model.predict(source=source, show=False, save=True)
+
+        r = results[0]
+        class_ids = r.boxes.cls.tolist()
+        counts = Counter(class_ids)
+        print(counts)
         suma = 0
-        for _, name in results[0].names.items():
-            suma += monety_voc.get(name)
-        print(suma)
-    else:
-        model.predict(source=source, show=False, save=True)
+
+        for class_id, count in counts.items():
+            class_name = r.names[int(class_id)]
+            value = monety_voc.get(class_name)
+            suma += value * count
+            print(f"{class_name}: {count}")
+        print(f"SUMA: {suma}")
+
+    elif mode == "video":
+        model.predict(source=source, show=True, save=True)
+    elif mode == "webcam":
+        model.predict(source=source, show=True, save=False)
 
 
 root = tk.Tk()
@@ -50,6 +62,7 @@ root.geometry("300x200")
 btn_image = tk.Button(root, text="Zdjęcie", command=analyze_image, width=25)
 btn_video = tk.Button(root, text="Wideo", command=analyze_video, width=25)
 btn_webcam = tk.Button(root, text="Webcam", command=analyze_webcam, width=25)
+btn_exit = tk.Button(root, text="Wyjście", command=root.destroy, width=25)
 
 btn_image.pack(pady=10)
 btn_video.pack(pady=10)
